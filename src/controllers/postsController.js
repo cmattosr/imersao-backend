3f8357ab33e8b5b1786ee17039e49ac7b3daa638
cmattosr <cmrocha@gmail.com>
2024-11-25 21:48:09 -0300

    ajuste para fazer o upload das imagens para o localhost:3000

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -43,8 +43,8 @@ export async function uploadImagem(req, res) {
 
 export async function atualizarNovoPost(req, res) {
     const id = req.params.id;
-    // const urlImagem = `https://localhost:3000/${id}.png`
-    const urlImagem = `https://imersao-backend-40446822135.southamerica-east1.run.app/${id}.png`
+    const urlImagem = `https://localhost:3000/${id}.png`
+    // const urlImagem = `https://imersao-backend-40446822135.southamerica-east1.run.app/${id}.png`
     try {
         const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
         const descricao = await gerarDescricaoComGemini(imgBuffer);
